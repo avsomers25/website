495ec239f8998996da67e352389ebfc788ad3fdd
Andrew Somers <avsomers25@gmail.com>
2023-03-05 13:45:36 -0500

    yeah

--- website/src/App.js
+++ website/src/App.js
@@ -1,21 +1,23 @@
-import logo from './logo.svg';
+import R from './R.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={R} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <br></br>
+          <br></br>
+          I'm a student at Rutgers studying CS
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/avsomers25"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Github
         </a>
       </header>
     </div>
