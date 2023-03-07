f3d0b827a56d733a485097093939c621cbfd4edd
Andrew Somers <avsomers25@gmail.com>
2023-03-07 14:06:52 -0500

    click and scroll

--- website/src/App.js
+++ website/src/App.js
@@ -1,10 +1,56 @@
 import R from './R.png';
 import './App.css';
 
+import {useRef} from "react";
+
 function Menu(){
+  const home = useRef(null);
+  const about = useRef(null);
+  const projects = useRef(null);
+  const resume = useRef(null);
+  const contact = useRef(null);
+
+  const scrollToSection = (elementRef) => {
+    window.scrollTo({
+      top: elementRef.current.offsetTop,
+      behavior: "smooth",
+    });
+  };
+
   return (
     <div>
-      crying
+      <div className = "color1">
+        <ul ref = {home}>
+            <li onClick={() => scrollToSection(home)} className="link">
+              Home
+            </li>
+            <li onClick={() => scrollToSection(about)} className="link">
+              About
+            </li>
+            <li onClick={() => scrollToSection(projects)} className="link">
+              Projects
+            </li>
+            <li onClick={() => scrollToSection(resume)} className="link">
+              Resume
+            </li>
+            <li onClick={() => scrollToSection(contact)} className="link">
+              Contact
+            </li>
+          </ul>
+          
+      </div>
+      <div ref={about} className="color2">
+        <h3>About</h3>
+      </div>
+      <div ref={projects} className="color3">
+        <h3>Projects</h3>
+      </div>
+      <div ref={resume} className="color4">
+        <h3>Resume</h3>
+      </div>
+      <div ref={contact} className="color5">
+        <h3>Contact</h3>
+      </div>
     </div>
   )
 }
