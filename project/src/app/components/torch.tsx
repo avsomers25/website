40de4d50c739be5f34f223b2340b5b9c431fd14f
avsomers25 <avsomers25@gmail.com>
2023-08-29 14:40:54 -0400

    asdadssada

--- project/src/app/components/torch.tsx
+++ project/src/app/components/torch.tsx
@@ -5,6 +5,9 @@ import torch from  'public/img/torch.png'
 
 export default function Torch(){
     return (
+        <div className = "">
+        <div className='shadow-[0_0px_100px_60px_rgba(255,0,255,1)]'></div>
         <Image className="object-none " src={torch} alt="" />
+        </div>
     )
 }
--- project/src/app/page.tsx
+++ project/src/app/page.tsx
@@ -26,7 +26,7 @@ export default function Home() {
           <div className="col-span-3 object-none">
             <Arch></Arch>
           </div>
-          <div className='shadow-[0_0px_100px_60px_rgba(255,0,255,1)]'>
+          <div className=''>
           <Torch></Torch>
           </div>
         </div>
