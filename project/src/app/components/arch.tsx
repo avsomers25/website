9424d46c829f4f6b4742fe95387251a81f896c49
avsomers25 <avsomers25@gmail.com>
2023-09-17 16:15:15 -0400

    torch glow actually centered

--- project/src/app/components/arch.tsx
+++ project/src/app/components/arch.tsx
@@ -3,8 +3,8 @@ import arch from  'public/img/arch.png'
 
 export default function Arch(){
     return (
-        <div>
-            <button className="z-10 absolute mt-24 ml-16 bg-violet-500 hover:bg-violet-600  hover:ring hover:ring-violet-300 ...">
+        <div  className = "flex items-center justify-center">
+            <button className="z-10 absolute bg-violet-500 hover:bg-violet-600  hover:ring hover:ring-violet-300 ...">
             Projects
             </button>
             <Image className = 'z-0'src={arch} alt=""/>
--- project/src/app/components/torch.tsx
+++ project/src/app/components/torch.tsx
@@ -5,8 +5,9 @@ import torch from  'public/img/torch.png'
 
 export default function Torch(){
     return (
-        <div className = "">
-        <div className='shadow-[0_0px_100px_60px_rgba(255,0,255,1)]'></div>
+        <div className = "flex items-center justify-center">
+        <div className='text-white absolute -mt-32 shadow-[0_0px_100px_60px_rgba(255,0,255,1)]'>
+        </div>
         <Image className="object-none " src={torch} alt="" />
         </div>
     )
